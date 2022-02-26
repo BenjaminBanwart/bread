bb36252871ea1ed1246cf9567da0b46ffa45208a
Benjamin Banwart <benjibanwart@gmail.com>
2022-02-26 14:09:42 -0600

    Create and New routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -17,6 +17,9 @@ function Index ({breads, title})  {
                     })
                 }
             </ul>
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
         </Default>
     )
 }
