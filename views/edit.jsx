c610b1e5a1407b78b6f6f3e8ea0b1f5056b45b6f
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-03-07 19:09:57 -0600

    Added new edit and delete functionality

--- views/edit.jsx
+++ views/edit.jsx
@@ -1,11 +1,11 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Edit ({bread, index}) {
+function Edit ({bread}) {
     return (
         <Default>
             <h2>Edit a bread</h2>
-            <form action={`/breads/${index}?_method=PUT`} method="POST">
+            <form action={`/breads/${bread.id}?_method=PUT`} method="POST">
                 <label htmlFor="name">Name</label>
                 <input
                     type="text"
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show ({bread, index}) {
+function Show ({bread}) {
     // Confirm we are getting our bread data in the terminal.
     // console.log(bread.name)
     return (
@@ -17,10 +17,10 @@ function Show ({bread, index}) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE"/>
             </form>
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
             <li><a href="/breads">Go home</a></li>
         </Default>
     )
