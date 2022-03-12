91cb9ba460db3971b26207be21af80ed7b8623d0
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-03-12 09:25:26 -0700

    Added ability to delete bakers with or without linked breads

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -30,5 +30,13 @@ baker.get('/:id', (req, res) => {
         })
 })
 
+// delete
+baker.delete('/:id', (req, res) => {
+    Baker.findByIdAndDelete(req.params.id) 
+        .then(deletedBaker => { 
+        res.status(303).redirect('/breads')
+    })
+})
+
 // export
 module.exports = baker                    
--- views/bakerShow.jsx
+++ views/bakerShow.jsx
@@ -19,6 +19,9 @@ function Show ({baker}) {
                     })
                 }
             </ul>
+            <form action={`/bakers/${baker.id}?_method=DELETE`} method="POST">
+                <input type="submit" value="DELETE"/>
+            </form>
         </Default>
     )
 }
